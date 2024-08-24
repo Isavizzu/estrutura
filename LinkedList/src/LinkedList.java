6c18dcbef1442a114382f7222e9f0c8372f6731e
Isabelle Vizzu <isabelle.vizzu@pucpr.edu.br>
2024-08-23 23:39:06 -0300

    commit do linkedList

--- LinkedList/src/LinkedList.java
+++ LinkedList/src/LinkedList.java
@@ -1,12 +1,15 @@
 import org.w3c.dom.Node;
 
+
 public class LinkedList<T> {
-    private Node<T> base;
-    private Node<T> top;
-    int size;
+    private No<T> base;
+    private No<T> top;
+    private int size;
+    private int indiceTop;
 
     public LinkedList(int size){
         this.size = size;
+        this.indiceTop = -1;
         base = null;
         top = null;
     }
@@ -16,7 +19,13 @@ public class LinkedList<T> {
         if (isFull()){
             throw new IllegalStateException("A lista está cheia!");
         }
-        this.data[indice++] = data;
+        if (base == null){
+            base = new No<>(null, null, data);
+            return;
+        }
+        No<T> noAnterior = getNo(indiceTop++).getAnterior();
+        top = new No<>(noAnterior, null, data);
+        noAnterior.setProximo(top);
     }
 
 
@@ -24,30 +33,31 @@ public class LinkedList<T> {
         if (isFull()){
             throw new IllegalStateException("A lista está cheia!");
         }
-        if (this.data[indice] == null) {
-            throw new IllegalStateException("Tem ao menos um índice antes do "+ pos + " livre!");
+        indiceTop++;
+        if (pos == 0){
+            No<T> noNovo = new No<>(null, base, data);
+            base.setAnterior(noNovo);
+            base = noNovo;
+            return;
         }
-        for (int i=indice;i>=0;i--){
-            if (pos == indice){
-                this.data[indice] = data;
-                break;
+        No<T> noProximo = getNo(pos);
+        No<T> noAnterior = getNo(pos).getAnterior();
+        No<T> noNovo = new No<>(noAnterior, noProximo, data);
+        noProximo.setAnterior(noNovo);
+        noAnterior.setProximo(noNovo);
+    }
+
+
+    public No<T> getNo(int pos) {
+        No<T> node = base;
+        int posAtual = 0;
+        while (true) {
+            if (posAtual == pos) {
+                return node;
             }
-            this.data[indice+1] = this.data[indice];
+            node = node.getProximo();
+            posAtual++;
         }
-        indice++;
-    }
-
-
-    public void setData(T data, int pos) {
-        if (this.data[pos] == null){
-            throw new  IllegalStateException("Esse indíce tem o valor nulo!");
-        }
-        this.data[pos] = data;
-    }
-
-
-    public T getData(int pos) {
-        return this.data[pos];
     }
 
 
@@ -56,42 +66,110 @@ public class LinkedList<T> {
     }
 
 
-    public int find(T data) {
-        int i = 0;
-        for (T t:this.data) {
-            i++;
-            if (t == data) {
-                return i;
-            }
-        }
-        throw new  IllegalStateException("Não existe um índice com o valor: " + data);
+    public T get (int pos) {
+        return getNo(pos).getValor();
     }
 
 
     public boolean isFull(){
-        return data[size - 1] != null;
+        return indiceTop+1 == size;
     }
 
 
     public T remove(int pos){
         if (isEmpty()){
-            throw new IllegalStateException("A lista já está vazia!");
+            throw new IllegalStateException("A lista encadeada já está vazia!");
         }
-        T valorRemovido = this.data[pos];
-        for (int i=pos;i<=indice;i++){
-            this.data[indice] = this.data[indice++];
+        indiceTop--;
+        if (pos == 0) {
+            No<T> noRemovido = base;
+            base = base.getProximo();
+            base.setAnterior(null);
+            return noRemovido.getValor();
         }
-        indice--;
-        return valorRemovido;
+        No<T> noRemovido = getNo(pos);
+        No<T> noAnterior = noRemovido.getAnterior();
+        No<T> noProximo = noRemovido.getProximo();
+        noAnterior.setProximo(noProximo);
+        noProximo.setAnterior(noAnterior);
+        return noRemovido.getValor();
+    }
+
+
+    public T remove(No<T> no) {
+        if (isEmpty()){
+            throw new IllegalStateException("A lista encadeada já está vazia!");
+        }
+        if (no.getAnterior() == null) {
+            base = base.getProximo();
+            base.setAnterior(null);
+            return no.getValor();
+        }
+        No<T> noAnterior = no.getAnterior();
+        No<T> noProximo = no.getProximo();
+        noAnterior.setProximo(noProximo);
+        noProximo.setAnterior(noAnterior);
+        return no.getValor();
     }
 
 
     public boolean isEmpty(){
-        return data[0] == null;
+        return base == null;
     }
 
 
     public void clear(){
-        data = (T[]) new Object[size];
+        this.indiceTop = -1;
+        base = null;
+        top = null;
+    }
+
+
+    public void set(int pos, T valor) {
+        getNo(pos).setValor(valor);
+    }
+}
+
+
+class No<T>{
+    private No<T> proximo;
+    private No<T> anterior;
+    private T valor;
+
+
+    public No (No<T> anterior, No<T> proximo, T valor) {
+        this.anterior = anterior;
+        this.valor = valor;
+        this.proximo = proximo;
+    }
+
+
+    public void setValor (T valor) {
+        this.valor = valor;
+    }
+
+
+    public No<T> getAnterior() {
+        return anterior;
+    }
+
+
+    public No<T> getProximo() {
+        return proximo;
+    }
+
+
+    public void setAnterior (No<T> anterior) {
+        this.anterior = anterior;
+    }
+
+
+    public void setProximo (No<T> proximo) {
+        this.proximo = proximo;
+    }
+
+
+    public T getValor () {
+        return valor;
     }
 }
